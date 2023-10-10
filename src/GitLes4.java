34567b7649e50a01f0f628b2b7cd4f369ee1bc05
Your Name <fdfdfss@fedsf.com>
2023-10-10 20:38:23 +0300

    add new system out println

--- src/GitLes4.java
+++ src/GitLes4.java
@@ -1,5 +1,6 @@
 public class GitLes4 {
     public static void main(String[] args) {
         System.out.println("Hello World and Git! i am Dima");
+        System.out.println("second commit to git ");
     }
 }
